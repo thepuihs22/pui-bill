0a722966b0a81ddf363231dbc31ad19ff5eb765c
Napaporn Utistham <utistham@th.ibm.com>
2025-04-16 17:25:52 +0700

    [Fe] fix superbase

--- app/api/share/route.ts
+++ app/api/share/route.ts
@@ -1,11 +1,11 @@
-import { createClient } from '@supabase/supabase-js';
+// import { createClient } from '@supabase/supabase-js';
 import { NextResponse } from 'next/server';
-
+import { supabase } from '@/lib/supabaseClient';
 // Initialize Supabase client
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+// const supabase = createClient(
+//   process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+// );
 
 interface SharePayload {
   orders: {
